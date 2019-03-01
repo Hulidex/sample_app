edecf180126fc91e97230ed087434cba40aebdb2
Jolu Izquierdo <joluizquierdo@pm.me>
2019-03-01 22:32:59 +0100

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User
 
 
   before_save {self.email = email.downcase}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
 
   
   validates :name, presence: true, length: {maximum: 50}
